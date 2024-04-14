5b7c63b47bc82fa5e40e3e43e4c3175a0bedbf1c
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-04-14 18:20:47 -0300

    ✨ New feature: estudando sobre manipulações de classes e styles, by Eduardo Xavier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,25 @@ export class AppComponent {
   inputText = 'Text Inicial Alterado';
   inputType = 'text';
   isDisabled = false;
+  buttonTitle = 'Titulo do botão';
+  buttonDisable = false;
+
+  widthButton1 = '110px';
+  bgButton1 = 'aqua';
+  widthButton2 = 130;
+
+  stylesObj = {
+    width: '160px',
+    backgroundColor: 'grey',
+  };
+  stylesObj2 = {
+    width: '160px',
+    backgroundColor: 'yellow',
+  };
+
+  isRedButton = true;
+  isGreenButton = true;
+
   enableDisableInput() {
     this.isDisabled = !this.isDisabled;
   }
@@ -29,5 +48,20 @@ export class AppComponent {
   handleInputEvent(event: Event) {
     const currentText = event.target as HTMLInputElement;
     this.inputText = currentText.value;
+  }
+
+  onButtonClick() {
+    this.buttonTitle = 'Titulo alterado';
+    this.buttonDisable = !this.buttonDisable;
+  }
+  updateStyleObj() {
+    this.stylesObj2.width = '170px';
+    this.stylesObj2.backgroundColor = 'lightblue';
+  }
+  updateStyleObjCorrect() {
+    this.stylesObj2 = {
+      width: '170px',
+      backgroundColor: 'lightblue',
+    };
   }
 }
